f7369c2603c64fa8096b225f03482838256c692d
gitvim <vim.09071@gmail.com>
2019-09-22 13:42:09 +0800

    * 留言部分增删改查完成

--- backend/main.go
+++ backend/main.go
@@ -13,12 +13,12 @@ import (
 // Comment 留言
 type Comment struct {
 	gorm.Model
-	Content    string
-	ArticleId  string
-	CommentUid string
-	ParentId   int
-	IsTop      uint
-	Status     uint
+	Content    string `json:"content" form:"content"`
+	ArticleId  string `json:"article_id" form:"article_id"`
+	CommentUid string `json:"comment_uid" form:"comment_uid"`
+	ParentId   int    `json:"parent_id" form:"parent_id"`
+	IsTop      int    `json:"is_top" form:"is_top"`
+	Status     int    `json:"status" form:"status"`
 }
 
 // User 用户表
@@ -41,6 +41,8 @@ func main() {
 	}
 	defer db.Close()
 
+	db.AutoMigrate(&Comment{})
+
 	r := gin.Default()
 
 	// todo 公众号信息
@@ -52,12 +54,14 @@ func main() {
 		})
 	})
 
+	// curl localhost:8080/comments?article_id=article_1
 	// 查询一篇文章中的留言列表
-	r.GET("/comment", func(c *gin.Context) {
+	r.GET("/comments", func(c *gin.Context) {
 		articleId := c.Query("article_id")
+		fmt.Println(articleId)
 
 		var comments = []Comment{}
-		if err := db.Where(&Comment{ArticleId: articleId}).Find(&comments).Error; err != nil {
+		if err := db.Where("article_id = ? ", articleId).Find(&comments).Error; err != nil {
 			fmt.Println(err)
 			c.JSON(1002, gin.H{
 				"message": "Query comment fail",
@@ -70,20 +74,39 @@ func main() {
 		})
 	})
 
+	// 单挑留言
+	// curl localhost:8080/comment/4 -X GET
+	r.GET("/comment/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		var comment Comment
+		if result := db.Where("id = ? ", id).First(&comment); result.Error != nil || !result.RecordNotFound() {
+			c.AbortWithStatus(404)
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"meesage": "Get record success.",
+			"comment": comment,
+		})
+	})
+
+	// curl localhost:8080/comment -X POST -d "content=3content&article_id=article_3&comment_uid=uid_3"
 	// 新增留言
 	r.POST("/comment", func(c *gin.Context) {
 		commentUid := c.PostForm("comment_uid")
 		articleId := c.PostForm("article_id")
 		content := c.PostForm("content")
-		//parentId := c.Query("parent_id")
+		parentId := c.GetInt("parent_id")
 
 		// do save comment
 		comment := &Comment{
 			Content:    content,
 			ArticleId:  articleId,
 			CommentUid: commentUid,
-			//ParentId:   parentId,
+			ParentId:   parentId,
 		}
+
+		// todo check parent comment id
+		// if notExistsParent  condition
 		if err := db.Create(&comment).Error; err != nil {
 			fmt.Println("Create comment fail", err)
 			c.JSON(1001, gin.H{
@@ -96,6 +119,45 @@ func main() {
 		})
 	})
 
+	// 更新留言，比如置顶, 不显示等
+	// curl localhost:8080/comment/8 -X PUT -d '{"is_top":1}'
+	r.PUT("/comment/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		var comment Comment
+		if result := db.Where("id = ? ", id).First(&comment); result.Error != nil || !result.RecordNotFound() {
+			c.AbortWithStatus(404)
+		}
+
+		c.BindJSON(&comment)
+		if err := db.Save(&comment).Error; err != nil {
+			c.JSON(1005, gin.H{
+				"message": "Update comment fail.",
+			})
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "Update comment success",
+			})
+		}
+	})
+
+	r.DELETE("/comment/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		var comment Comment
+		if result := db.Where("id = ? ", id).First(&comment); result.Error != nil || !result.RecordNotFound() {
+			c.AbortWithStatus(404)
+		}
+
+		if err := db.Delete(&comment).Error; err != nil {
+			c.JSON(1005, gin.H{
+				"message": "Delete comment fail.",
+			})
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "Delete comment success",
+			})
+		}
+
+	})
 	// todo 留言置顶
 
 	// 回复也作为
