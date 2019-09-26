ab819d4ab8539d4d6124909353c900f47028fb36
yangguang_chen <yangguang_chen@kingdee.com>
2019-09-26 14:25:26 +0800

    + 新增中间件，用于校验access_token和请求标记, 增加路由分组

---
+++ backend/middleware/auth.go
@@ -0,0 +1,34 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/satori/go.uuid"
+)
+
+func RequestIdMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		random, _ := uuid.NewV4()
+
+		ctx.Writer.Header().Set("X-Request-ID", random.String())
+		ctx.Next()
+	}
+}
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		accessToken := ctx.Request.FormValue("access_token")
+		if accessToken == "" {
+			responseWithoutAuth(401, "Required grant token", ctx)
+		}
+		ctx.Next()
+	}
+}
+
+func responseWithoutAuth(code int, message interface{}, ctx *gin.Context) {
+	resp := map[string]interface{}{
+		"code":    code,
+		"message": message,
+	}
+	ctx.JSON(code, resp)
+	ctx.Abort()
+}
--- backend/router/router.go
+++ backend/router/router.go
@@ -1,15 +1,16 @@
 package router
 
 import (
-	"github.com/chenyangguang/WeChat-Official-Accounts-Comment/backend/controller"
+	"github.com/chenyangguang/WeChat-Official-Accounts-Comment/backend/middleware"
 	"github.com/gin-gonic/gin"
+
+	"github.com/chenyangguang/WeChat-Official-Accounts-Comment/backend/controller"
 	"io"
 	"log"
 	"os"
 )
 
-func InitRouter() *gin.Engine {
-
+func initLogfile() {
 	logFile, err := os.Create("gin.log")
 	if err != nil {
 		panic(err)
@@ -17,13 +18,20 @@ func InitRouter() *gin.Engine {
 	gin.DefaultWriter = io.MultiWriter(logFile, os.Stdout)
 	log.SetOutput(gin.DefaultWriter)
 
-	r := gin.Default()
+}
+func InitRouter() *gin.Engine {
+	initLogfile()
 
-	r.GET("/comments", controller.GetAllComments)
-	r.GET("/comment/:id", controller.GetCommentById)
-	r.POST("/comment", controller.CreateComment)
-	r.PUT("/comment/:id", controller.UpdateComment)
-	r.DELETE("/comment/:id", controller.DeleteComment)
+	r := gin.Default()
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/comments", controller.GetAllComments)
+		v1.GET("/comment/:id", controller.GetCommentById)
+		v1.POST("/comment", controller.CreateComment)
+		v1.PUT("/comment/:id", controller.UpdateComment)
+		v1.DELETE("/comment/:id", controller.DeleteComment)
+	}
+	v1.Use(middleware.AuthMiddleware(),middleware.RequestIdMiddleware())
 
 	return r
 
